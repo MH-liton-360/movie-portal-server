245e030389a7e641aa94e60ab8d18e46c0ff376d
MH-liton-360 <liton72524nk@gmail.com>
2025-04-23 23:50:32 +0600

    Done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,12 @@ async function run() {
 
         const movieCollection = client.db('movieDB').collection('movie');
 
+        app.get('/movie', async (req, res) => {
+            const cursor = movieCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
 
         app.post('/movie', async (req, res) => {
             const newMovie = req.body;
@@ -41,7 +47,12 @@ async function run() {
             res.send(result);
         })
 
-
+        app.delete('/movie/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await movieCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
